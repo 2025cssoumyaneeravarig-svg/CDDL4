62676684eb887b5e6285c6a590d7449f11570135
Suhani33 <2025cs_suhaninagappagouda_g@nie.ac.in>
2026-09-22 10:15:00 +0530

    College name added

--- name.java
+++ name.java
@@ -4,9 +4,12 @@ public class  name{
     String name="soumya";
     String usn="2025cs199";
     int marks = 88;
+    String College="NIE_Mysore";
+
     System.out.println("Name:" +name);
     System.out.println("USN:" +usn);
     System.out.println("Marks:" +marks);
+    System.out.println("College:" +College);
 
   }
 }
