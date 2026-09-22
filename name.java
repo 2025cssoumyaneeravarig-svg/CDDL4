13e57faa7fe302292e680084050b31fd844bb8ae
2025cssoumyaneeravarig-svg <2025cs_soumyaneeravari_g@nie.ac.in>
2026-09-22 09:56:49 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,12 @@
+public class  name{
+  public static void main(String[] args)
+  {
+    String name="soumya";
+    String usn="2025cs199";
+    int marks = 88;
+    System.out.println("Name:" +name);
+    System.out.println("USN:" +usn);
+    System.out.println("Marks:" +marks);
+
+  }
+}
